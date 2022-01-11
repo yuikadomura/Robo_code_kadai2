5cfa04f6a01e4f9a425c26c5d1aaac4205a73f77
Yui Kadomura <yui314jumping@gmail.com>
2022-01-11 01:45:53 +0000

    Update

--- scripts/diffTime.py
+++ scripts/diffTime.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #BSD 3-Clause Licence
-#Copyright (c) 2021 Ryuichi Ueda + Yui Kadomura. All rights reserved.
+#Copyright (c) 2021 Yui Kadomura. All rights reserved.
 
 import rospy
 from std_msgs.msg import Float64, String
